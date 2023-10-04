a33efa5b6ec33276f9b3ae3499eccb06bcea5589
Anubhav singh <anubhavsinghnit5132@gmail.com>
2023-10-05 00:22:10 +0530

    updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
         <img src={Logo} alt="logo" width='200px' height='100px'/>
         <Typography variant='h5' pb='40px' mt='20px'>
-         All Rights Reserved
+        Copyright © 2023 Anubhav Singh | All Rights Reserved
         </Typography>
       </Stack>
     </Box>
